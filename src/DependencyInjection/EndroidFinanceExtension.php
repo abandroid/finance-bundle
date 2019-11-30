ae4f86545a3af30d48e10f779077e6b1af5239be
Jeroen van den Enden <info@endroid.nl>
2019-11-30 09:28:52 +0000

    Update workflow

--- src/DependencyInjection/EndroidFinanceExtension.php
+++ src/DependencyInjection/EndroidFinanceExtension.php
@@ -13,10 +13,10 @@ namespace Endroid\FinanceBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class EndroidFinanceExtension extends Extension implements PrependExtensionInterface
 {
